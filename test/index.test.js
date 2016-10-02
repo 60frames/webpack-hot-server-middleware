636c9e5e99fded5ccd5a30dee007b91c1c426da0
Richard <riscarrott@googlemail.com>
2016-10-02 15:24:37 +0100

    fixup! Add integration tests

--- test/index.test.js
+++ test/index.test.js
@@ -16,120 +16,159 @@ const incorrectServerCompilerNameConfig = require('./fixtures/incorrectservercom
 const incorrectClientCompilerNameConfig = require('./fixtures/incorrectclientcompilername/webpack.config.js');
 const badExportConfig = require('./fixtures/badexport/webpack.config.js');
 
-function createServer(config, options) {
+function createServer(config, mountWebpackDevMiddleware = true) {
     const compiler = webpack(config);
     const app = express();
-    app.use(webpackDevMiddleware(compiler, {
-        quiet: true
-    }));
-    app.use(webpackHotServerMiddleware(compiler, options));
+    let webpackDev;
+    if (mountWebpackDevMiddleware) {
+        webpackDev = webpackDevMiddleware(compiler, {
+            quiet: true
+        });
+        app.use(webpackDev);
+    }
+    app.use(webpackHotServerMiddleware(compiler));
     app.use((err, req, res, next) => {
         res.status(500).send(err.toString());
     });
-    return app;
+    return [app, (cb) => {
+        // HACK: Process won't terminate unless close is successful however
+        // chokidar has a bug whereby calling close before the watcher is ready
+        // fails so having to wait for...a bit.
+        // https://github.com/webpack/webpack/issues/1920
+        // https://github.com/paulmillr/chokidar/pull/536
+        setTimeout(() => {
+            if (webpackDev) {
+                webpackDev.close(cb);
+                return;
+            }
+            if (cb) {
+                cb();
+            }
+        }, 100);
+    }];
 }
 
 describe('index', () => {
 
     it('throws when the compiler isn\'t a `MultiCompiler`', () => {
-        expect(createServer.bind(null, noMultiCompilerConfig)).toThrow(
+        // Avoid mounting webpackDevMiddleware as we expect it to throw so would
+        // lose the opportunity to close the connection.
+        expect(createServer.bind(null, noMultiCompilerConfig, false)).toThrow(
             new Error(`Expected webpack compiler to contain both a 'client' and 'server' config`)
         );
     });
 
     it('throws when server compiler cannot be found', () => {
-        expect(createServer.bind(null, incorrectServerCompilerNameConfig)).toThrow(
+        expect(createServer.bind(null, incorrectServerCompilerNameConfig, false)).toThrow(
             new Error(`Expected a webpack compiler named 'server'`)
         );
     });
 
     it('throws when client compiler cannot be found', () => {
-        expect(createServer.bind(null, incorrectClientCompilerNameConfig)).toThrow(
+        expect(createServer.bind(null, incorrectClientCompilerNameConfig, false)).toThrow(
             new Error(`Expected a webpack compiler named 'client'`)
         );
     });
 
     it('handles commonJs exports', done => {
-        const app = createServer(commonJsConfig);
+        const [app, close] = createServer(commonJsConfig);
         request(app)
             .get('/')
             .expect(200)
             .expect('Hello Server')
             .end((err, res) => {
-                if (err) {
-                    done.fail(err);
-                }
-                done();
+                close(() => {
+                    if (err) {
+                        done.fail(err);
+                        return;
+                    }
+                    done();
+                });
             });
     });
 
     it('handles es modules exports', done => {
-        const app = createServer(esModulesConfig);
+        const [app, close] = createServer(esModulesConfig);
         request(app)
             .get('/')
             .expect(200)
             .expect('Hello Server')
             .end((err, res) => {
-                if (err) {
-                    done.fail(err);
-                }
-                done();
+                close(() => {
+                    if (err) {
+                        done.fail(err);
+                        return;
+                    }
+                    done();
+                });
             });
     });
 
     it('handles source maps', done => {
-        const app = createServer(sourceMapConfig);
+        const [app, close] = createServer(sourceMapConfig);
         request(app)
             .get('/')
             .expect(200)
             .expect('Hello Server')
             .end((err, res) => {
-                if (err) {
-                    done.fail(err);
-                }
-                done();
+                close(() => {
+                    if (err) {
+                        done.fail(err);
+                        return;
+                    }
+                    done();
+                });
             });
     });
 
     it('handles compile time errors', done => {
-        const app = createServer(compiletimeErrorConfig);
+        const [app, close] = createServer(compiletimeErrorConfig);
         request(app)
             .get('/')
             .expect(500)
             .expect(/ModuleParseError/)
             .end((err, res) => {
-                if (err) {
-                    done.fail(err);
-                }
-                done();
+                close(() => {
+                    if (err) {
+                        done.fail(err);
+                        return;
+                    }
+                    done();
+                });
             });
     });
 
     it('handles runtime errors', done => {
-        const app = createServer(runtimeErrorConfig);
+        const [app, close] = createServer(runtimeErrorConfig);
         request(app)
             .get('/')
             .expect(500)
             .expect('Error: ¯\\_(ツ)_/¯')
             .end((err, res) => {
-                if (err) {
-                    done.fail(err);
-                }
-                done();
+                close(() => {
+                    if (err) {
+                        done.fail(err);
+                        return;
+                    }
+                    done();
+                });
             });
     });
 
     it(`handles bad server exports`, done => {
-        const app = createServer(badExportConfig);
+        const [app, close] = createServer(badExportConfig);
         request(app)
             .get('/')
             .expect(500)
             .expect(`Error: The 'server' compiler must export a function in the form of \`(stats) => (req, res, next) => void 0\``)
             .end((err, res) => {
-                if (err) {
-                    done.fail(err);
-                }
-                done();
+                close(() => {
+                    if (err) {
+                        done.fail(err);
+                        return;
+                    }
+                    done();
+                });
             });
     });
 
