5cbd767dc1bc9b97eef14d0e3224c9f39cd0c0de
Mohsen Azimi <me@azimi.me>
2018-01-17 13:54:30 -0800

    Do not `declare` modules when they are used in ambient context

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,19 @@
-declare module 'webpack-hot-server-middleware' {
-  import { RequestHandler } from 'express';
-  // tslint:disable-next-line no-implicit-dependencies
-  import { MultiCompiler, Stats } from 'webpack';
-  type Options<T = Record<string, any>> = Partial<{
-    /** The name of the server entry point, defaults to 'main'. */
-    chunkName: string;
+import { RequestHandler } from 'express';
+// tslint:disable-next-line no-implicit-dependencies
+import { MultiCompiler, Stats } from 'webpack';
+type Options<T = Record<string, any>> = Partial<{
+  /** The name of the server entry point, defaults to 'main'. */
+  chunkName: string;
 
-    /** object Mixed in with clientStats & serverStats and passed to the serverRenderer. */
-    serverRendererOptions: T;
+  /** object Mixed in with clientStats & serverStats and passed to the serverRenderer. */
+  serverRendererOptions: T;
 
-    createHandler(error: boolean, serverRenderer: RequestHandler): RequestHandler;
-  }>;
+  createHandler(error: boolean, serverRenderer: RequestHandler): RequestHandler;
+}>;
 
-  declare function webpackHotServerMiddleware<T>(
-    compiler: MultiCompiler,
-    options?: Options<T>,
-  ): RequestHandler;
+declare function webpackHotServerMiddleware<T>(
+  compiler: MultiCompiler,
+  options?: Options<T>,
+): RequestHandler;
 
-  export = webpackHotServerMiddleware;
-}
+export = webpackHotServerMiddleware;
